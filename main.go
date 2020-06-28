585575e4afd89f808ad4d000bd190aa3cf978e00
seri1234 <your.email@example.com>
2020-06-29 06:32:08 +0900

    viewの追加

--- main.go
+++ main.go
@@ -1,14 +1,46 @@
 package main
 
 import (
-  "net/http"
-  "github.com/labstack/echo"
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo"
 )
 
+//echo.Renderer interface
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
-  e := echo.New()
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+
+	e := echo.New()
+
+	e.Renderer = t
+
 	e.GET("/", func(c echo.Context) error {
-    return c.String(http.StatusOK, "Hello, Echo World!!")
-  })
-  e.Logger.Fatal(e.Start(":8080"))
+		return c.String(http.StatusOK, "Hello, Echo World!!")
+	})
+
+	e.GET("/index", func(c echo.Context) error {
+		// テンプレートに渡す値
+
+		data := struct {
+			Content_a string
+		}{
+
+			Content_a: "雨が降っています。",
+		}
+		return c.Render(http.StatusOK, "index", data)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
